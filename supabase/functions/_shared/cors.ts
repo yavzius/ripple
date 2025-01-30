d5f6f0148387abb3905002f8d932bc0207ad88d4
yavzius <yavuzacc@gmail.com>
2025-01-29 19:35:44 -0500

    update:cors

--- supabase/functions/_shared/cors.ts
+++ supabase/functions/_shared/cors.ts
@@ -1,5 +1,5 @@
 export const corsHeaders = {
-  'Access-Control-Allow-Origin': 'http://localhost:8080',
+  'Access-Control-Allow-Origin': 'http://localhost:8080,https://ripplecrm.vercel.app',
   'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type, x-supabase-auth',
   'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
   'Access-Control-Allow-Credentials': 'true',
